56a9f6753491bf77f047017086474930aab34841
Sunil <turpati.sunil@gmail.com>
2025-04-20 14:09:47 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""_summary_ """
+def main(name: str):
+    """_summary_
+
+    Args:
+        name (str): _description_
+    """
+    print(f"Hello :{name}!")
+
+if __name__ == "__main__":
+
+    main("Sunil")
